19dc7210bae10ce07bef4730712c693b7ded34ce
Zynh Ludwig <Zynh0722@gmail.com>
2022-10-28 19:11:58 -0700

    fuck react strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,7 @@ import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
-  // </React.StrictMode>
+  <ChakraProvider>
+    <App />
+  </ChakraProvider>
 )
